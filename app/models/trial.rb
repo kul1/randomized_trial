cc25b4d1f9e6f3119cb5ba6f665e3cdecc2a5b8e
P Kul <1.0@kul.asia>
2018-01-02 12:31:49 -0600

    Fixed date

--- app/models/trial.rb
+++ app/models/trial.rb
@@ -1,12 +1,12 @@
 # encoding: utf-8
 class Trial
   include Mongoid::Document
+
   # jinda begin
   include Mongoid::Timestamps
   field :title, :type => String
   field :status, :type => String
-  belongs_to :user
   field :estart, :type => Date
   field :eend, :type => Date
-  # jinda end
+  belongs_to :user
 end
